a1069505a7a5b0a7353580385fe60605b06508ec
dananguo <190285975@qq.com>
2020-01-06 20:01:33 +0800

    modify image

--- pages/highlight.vue
+++ pages/highlight.vue
@@ -4,21 +4,23 @@
       <div class="left-nav">
         <div class="float-nav" id="float-nav">
           <ul class="sui-nav nav-tabs nav-xlarge tab-navbar tab-vertical">
-            <nuxt-link to="/highlight/recommend" tag="li" active-class="active" v-if="user.name !== undefined " exact><a>推荐</a></nuxt-link>
+            <nuxt-link to="/highlight/recommend" tag="li" active-class="active" v-if="user.name !== undefined " exact>
+              <a>推荐</a></nuxt-link>
             <nuxt-link to="/highlight/channel/0" tag="li" active-class="active" exact><a>热门</a></nuxt-link>
-            <nuxt-link tag="li" :to="`/highlight/channel/${item.id}`" active-class="active" v-for="(item,index) in channellist" :key="index"><a>{{item.name}}</a></nuxt-link>
+            <nuxt-link tag="li" :to="`/highlight/channel/${item.id}`" active-class="active"
+                       v-for="(item,index) in channellist" :key="index"><a>{{item.name}}</a></nuxt-link>
           </ul>
         </div>
       </div>
-        <nuxt-child/>
+      <nuxt-child/>
       <div class="right-content">
         <div class="fl right">
           <div class="activity">
             <div class="acti">
-              <img :src="this.img[0]" alt="活动一"/>
+              <img src="http://q2tjj3jyx.bkt.clouddn.com/huawei1.jpg" alt="活动一"/>
             </div>
             <div class="acti">
-              <img :src="this.img[1]" alt="活动二"/>
+              <img src="http://q2tjj3jyx.bkt.clouddn.com/ITcreation.png" alt="活动二"/>
             </div>
           </div>
           <div class="block-btn">
@@ -29,9 +31,11 @@
             <h3 class="title">热门回答</h3>
             <div class="lists">
               <ul>
-                <li class="list-item" v-for="(item,index) in hotlist" :key="index"><a :href="`/qa/item/${item.id}`"><p class="list-title">{{item.title}}</p></a>
-                  <p class="authorInfo"><span class="authorName"><img src="~/assets/img/widget-photo.png"
-                                                                      alt=""/>{{item.nickname}}</span> <span>{{item.createtime}}</span></p>
+                <li class="list-item" v-for="(item,index) in hotlist" :key="index"><a :href="`/qa/item/${item.id}`"><p
+                  class="list-title">{{item.title}}</p></a>
+                  <p class="authorInfo"><span class="authorName"><img :src="avatar"
+                                                                      alt=""/>{{item.nickname}}</span> <span>{{item.createtime}}</span>
+                  </p>
                 </li>
               </ul>
               <nuxt-link to="/qa"><a class="sui-btn btn-block btn-bordered btn-more">查看更多</a></nuxt-link>
@@ -41,12 +45,15 @@
             <h3 class="title">活动日历</h3>
             <div class="list">
               <ul>
-                <li class="list-item" v-for="(item,index) in gatheringlist" :key="index"> <p class="list-time"> {{item.starttime}} {{item.city}}</p>
-                  <a :href="`/gathering/item/${item.id}`"><div class="list-content clearfix">
-                    <div class="fl img">
-                      <img :src="item.image" alt=""/>
+                <li class="list-item" v-for="(item,index) in gatheringlist" :key="index"><p class="list-time">
+                  {{item.starttime}} {{item.city}}</p>
+                  <a :href="`/gathering/item/${item.id}`">
+                    <div class="list-content clearfix">
+                      <div class="fl img">
+                        <img :src="item.image" alt=""/>
+                      </div>
                     </div>
-                  </div></a>
+                  </a>
                   <div>
                     <p>{{item.summary}}</p>
                   </div>
@@ -62,38 +69,38 @@
 </template>
 
 <script>
-    import '~/assets/css/page-headline-logined.css'
-    import channelApi from "@/api/channel";
-    import problemApi from "@/api/problem";
-    import gatheringApi from "@/api/gathering";
-    import imageApi from "@/api/image";
-    import axios from 'axios';
-    import Auth from "@/utils/auth";
-    import user from "@/api/user";
+  import '~/assets/css/page-headline-logined.css'
+  import channelApi from "@/api/channel";
+  import problemApi from "@/api/problem";
+  import gatheringApi from "@/api/gathering";
+  import imageApi from "@/api/image";
+  import axios from 'axios';
+  import Auth from "@/utils/auth";
 
-    export default {
-        data(){
-            return{
-                user:{},
-            }
-        },
-        computed(){
+  export default {
+    data() {
+      return {
+        user: {},
+        avatar: "http://q2tjj3jyx.bkt.clouddn.com/widget-myphoto.jpg"
+      }
+    },
+    computed() {
 
-        },
-        created() {
-            this.user = Auth.getUser();
-        },
-        asyncData(){
-            return axios.all([channelApi.getList(),problemApi.list("hotlist",0,1,5),gatheringApi.search(1,4,{"state":"1"}),imageApi.getImgs()]).then(axios.spread((res1,res2,res3,res4)=>{
-                return {
-                    channellist:res1.data.data.rows,
-                    hotlist:res2.data.data.rows,
-                    gatheringlist:res3.data.data.rows,
-                    img:res4.data.data,
-                    user: Auth.getUser()
-                }
-            }))
-        },
-    }
+    },
+    created() {
+      this.user = Auth.getUser();
+    },
+    asyncData() {
+      return axios.all([channelApi.getList(), problemApi.list("hotlist", 0, 1, 5), gatheringApi.search(1, 4, {"state": "1"}), imageApi.getImgs()]).then(axios.spread((res1, res2, res3, res4) => {
+        return {
+          channellist: res1.data.data.rows,
+          hotlist: res2.data.data.rows,
+          gatheringlist: res3.data.data.rows,
+          img: res4.data.data,
+          user: Auth.getUser()
+        }
+      }))
+    },
+  }
 </script>
 
--- pages/highlight/channel/_id.vue
+++ pages/highlight/channel/_id.vue
@@ -12,7 +12,7 @@
         <ul class="headline fixed" id="headline">
           <li class="headline-item" v-for="(item,index) in articles" :key="index">
             <div class="fl indexImg">
-              <img :src="item.image"/> <!--todo-->
+<!--              <img :src="author"/>-->
             </div>
             <nuxt-link :to="`/article/item/${item.id}`">
               <div class="words">
@@ -31,102 +31,101 @@
         <ul id="loaded" class="headline">
         </ul>
       </div>
-       <script src="~/assets/data-list.js" type="text/javascript"></script>
+      <!--       <script src="~/assets/data-list.js" type="text/javascript"></script>-->
     </div>
   </div>
 </template>
 
 <script>
-    import '@/assets/css/page-headline-logined.css'
-    import articleApi from "@/api/article";
-    import imageApi from "@/api/image";
-    import axios from 'axios';
+  import '@/assets/css/page-headline-logined.css'
+  import articleApi from "@/api/article";
+  import imageApi from "@/api/image";
+  import axios from 'axios';
 
-    export default {
-        name: "_id",
-        data() {
-            return {
-                curPage: 1,
-                busy: false,
-                imgList: {
-                  img1: "http://q2tjj3jyx.bkt.clouddn.com/1.png",
-                  img2: "http://q2tjj3jyx.bkt.clouddn.com/2.png",
-                  img3: "http://q2tjj3jyx.bkt.clouddn.com/3.png"
-                }
-            }
+  export default {
+    name: "_id",
+    data() {
+      return {
+        curPage: 1,
+        busy: false,
+        imgList: {
+          img1: "http://q2tjj3jyx.bkt.clouddn.com/1.png",
+          img2: "http://q2tjj3jyx.bkt.clouddn.com/2.png",
+          img3: "http://q2tjj3jyx.bkt.clouddn.com/3.png"
         },
-        mounted() {
-            window.addEventListener('scroll', this.handleScroll, true)
-        },
-        asyncData({params, query}) {
-            let searchmap={};
-            let arr = Object.keys(query)
-            console.log(query !== undefined);
-            console.log(query);
-            if (params.id == 0) {
-                if (arr.length !== 0){
-                    // console.log(1)
-                    searchmap = {'title': query.keywords};
-                }
-                else{
-                    // console.log(2)
-                    searchmap = {"istop": "1"};
-                }
-            } else {
-                if (arr.length !== 0){
-                    // console.log(params.id.toString() + '1');
-                    searchmap = {'title': query.keywords};
-                }
-                else{
-                    // console.log(params.id.toString() + '2');
-                    searchmap = {"channelid": params.id.toString()}
-                }
-            }
-            return axios.all([articleApi.getList(1, 10, searchmap), imageApi.getImgs()]).then(
-                axios.spread((res, res2) => {
-                    return {
-                        articles: res.data.data.rows,
-                        channelId: params.id,
-                        img: res2.data.data,
-                        query: query
-                    }
-                })
-            )
-        },
-        methods: {
-            handleScroll() {
-                let _self = this;
-                if (window.scrollY >= document.body.scrollHeight - document.body.clientHeight - 20) {
-                    if (!_self.busy) {
-                        _self.busy = true;
-                        _self.loadMore();
-                    }
-                }
-            },
-            loadMore() {
-                let searchmap={};
-                let arr = Object.keys(this.query)
-
-                if (this.channelId == 0) {
-                    if (arr.length !== 0)
-                        searchmap = {'title': query.keywords};
-                    else
-                        searchmap = {"istop": "1"};
-                } else {
-                    if (arr.length !== 0)
-                        searchmap = {'title': query.keywords};
-                    else
-                        searchmap = {"channelid": params.id.toString()}
-                }
-                this.curPage++;
-                articleApi.getList(this.curPage, 10, searchmap).then(response => {
-                    this.articles = this.articles.concat(response.data.data.rows);
-                    this.busy = false
-                })
-
-            }
+        // author: "http://q2tjj3jyx.bkt.clouddn.com/widget-myphoto.jpg"
+      }
+    },
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll, true)
+    },
+    asyncData({params, query}) {
+      let searchmap = {};
+      let arr = Object.keys(query)
+      console.log(query !== undefined);
+      console.log(query);
+      if (params.id == 0) {
+        if (arr.length !== 0) {
+          // console.log(1)
+          searchmap = {'title': query.keywords};
+        } else {
+          // console.log(2)
+          searchmap = {"istop": "1"};
         }
+      } else {
+        if (arr.length !== 0) {
+          // console.log(params.id.toString() + '1');
+          searchmap = {'title': query.keywords};
+        } else {
+          // console.log(params.id.toString() + '2');
+          searchmap = {"channelid": params.id.toString()}
+        }
+      }
+      return axios.all([articleApi.getList(1, 10, searchmap), imageApi.getImgs()]).then(
+        axios.spread((res, res2) => {
+          return {
+            articles: res.data.data.rows,
+            channelId: params.id,
+            img: res2.data.data,
+            query: query
+          }
+        })
+      )
+    },
+    methods: {
+      handleScroll() {
+        let _self = this;
+        if (window.scrollY >= document.body.scrollHeight - document.body.clientHeight - 20) {
+          if (!_self.busy) {
+            _self.busy = true;
+            _self.loadMore();
+          }
+        }
+      },
+      loadMore() {
+        let searchmap = {};
+        let arr = Object.keys(this.query)
+
+        if (this.channelId == 0) {
+          if (arr.length !== 0)
+            searchmap = {'title': query.keywords};
+          else
+            searchmap = {"istop": "1"};
+        } else {
+          if (arr.length !== 0)
+            searchmap = {'title': query.keywords};
+          else
+            searchmap = {"channelid": params.id.toString()}
+        }
+        this.curPage++;
+        articleApi.getList(this.curPage, 10, searchmap).then(response => {
+          this.articles = this.articles.concat(response.data.data.rows);
+          this.busy = false
+        })
+
+      }
     }
+  }
 </script>
 
 <style scoped>
